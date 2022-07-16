112fe110fc4c7a8d5c115d88934a0e4fb3d44a38
Nikita-Kuzhl <kuzhleev@yandex.ru>
2022-07-16 11:40:12 +0300

    task_4

---
+++ lottery.js
@@ -0,0 +1,20 @@
+function lottery() {
+  console.log("Вы начали игру");
+  let promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      Math.random(0) > 0.5 ? resolve() : reject("Вы промахнулись");
+    }, 2000);
+  });
+  return promise;
+}
+
+const result = async () => {
+  try {
+    await lottery();
+    console.log("Вы выиграли");
+  } catch (e) {
+    console.log("Вы проиграли");
+  }
+  console.log("Игра закончена");
+};
+result();
